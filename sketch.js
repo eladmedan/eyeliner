938af9e6f957f78a587b3047c92926b09a02e33e
eladmedan <eladmedan@gmail.com>
2022-03-27 21:55:44 +0300

    and again - saved all files

--- sketch.js
+++ sketch.js
@@ -23,7 +23,7 @@ function setup() {
 //     if (data == null) {
 //         return;
 //     }
-//      xprediction = data.x; //these x coordinates are relative to the viewport 
+//      xprediction = data.x; //these x coordinates are relative to the viewport
 //      yprediction = data.y; //these y coordinates are relative to the viewport
 //     console.log("xprediction",xprediction);
 //     console.log("yprediction",yprediction);
@@ -32,53 +32,53 @@ function setup() {
 // }).begin();
 // frameRate(120);
   pos = 0.5 * windowWidth - 78148;
-  
+
   maxSpeed = 10;
-  
+
   content = select('#target');
-  
+
   content.style('white-space', 'nowrap');
-  
+
   // let contentWidth = content.clientWidth;
-  
-      
+
+
   content.position(pos, 0.3 * windowHeight);
-  
+
   results = Splitting(); //array of all the splits in #target
   wordsArray = selectAll('.word'); //array of all .word class
 
   lastWord = 0;
 
-    //adds hover event listener for each word 
+    //adds hover event listener for each word
   for (let c_idx = 0; c_idx < results[0].words.length; c_idx++) {
-    
+
     results[0].words[c_idx].addEventListener("mouseover", function(e) {
     //add the click event listener that will trigger the following:
-      
+
       //1. get the current word's index number
       styleVal = getComputedStyle(e.target, 'style');
       thisWord = (int(styleVal.getPropertyValue('--word-index')));
-       
+
       //2. cleans up the grades from text - MAYBE MAKE MORE EFFICIENT
       if (lastWord > 0) {
-      for (let i = lastWord - 1; i < lastWord + 3; i++){ 
+      for (let i = lastWord - 1; i < lastWord + 3; i++){
        wordsArray[i].style('font-weight' ,400);
       }
     }
-      
-      //3. grades the text where clicked
+
+      //3. grades the text where hovered
         wordsArray[thisWord - 1].style('font-weight' ,600);
         wordsArray[thisWord].style('font-weight' ,700);
         wordsArray[thisWord + 1].style('font-weight' ,600);
         wordsArray[thisWord + 2].style('font-weight' ,500);
-       
+
         lastWord = thisWord
       })
   }
-      
-      
-      
-  
+
+
+
+
 
 }
 
@@ -87,8 +87,8 @@ function draw() {
   move = map(mouseX, 0, 0.45 * windowWidth, maxSpeed, 0);
   } else if (mouseX > 0.55 * windowWidth) {
     move = map(mouseX, 0.55 * windowWidth, windowWidth, 0, -maxSpeed);
-  } else move = 0;  
-  
+  } else move = 0;
+
   pos = pos + move;
   content.position(pos, 0.3 * windowHeight);
 }
